5e0a72febc12acf8626c40f6d459739a7b847e26
Mohamad44p <mohamad44p@yahoo.com>
2024-07-30 16:18:50 +0300

    edites

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable jsx-a11y/alt-text */
 "use client";
 
 import {
@@ -24,31 +25,100 @@ export default function Home() {
 
   const divRadius = useTransform(scrollYProgressSpring, [0, 1], [100, 0]);
   const divClip = useMotionTemplate`circle(${divRadius}% at 50% 50%)`;
+  const scale = useTransform(scrollYProgress, [0, 0.05], [0.8, 1]);
+  const xRow1 = useTransform(scrollYProgress, [0.15, 0.83], [0, -300]);
+  const xRow2 = useTransform(scrollYProgress, [0.15, 0.83], [0, 300]);
+  const xRow3 = useTransform(scrollYProgress, [0.15, 0.83], [0, -400]);
 
   return (
-    <main>
-      <div ref={ref} className="relative top-0 z-10 h-[200vh] w-full">
-        <motion.div
-          style={{ clipPath: divClip }}
-          className="sticky hero w-full left-0 top-0 grid  origin-[50%_50%] gap-2"
-        >
-          <video
-            autoPlay
-            loop
-            muted
-            playsInline
-            className="object-cover w-full h-full"
-            src="/Y2meta.app-THIS IS 4K MARVEL (Ultra HD)-(1080p60).mp4"
-          />
-        </motion.div>
-      </div>
-      <div className="mt-[-200vh] h-[190vh] overflow-clip pb-20">
-        <motion.p
-          className="relative top-1/2 mx-auto md:flex items-center justify-center block aspect-video w-[1600px] max-w-[90%] rounded-[60px] shadow-2xl md:top-1/4"
-        >
-          some text
-        </motion.p>
+    <main className="main w-full">
+      <div ref={ref} className="home section w-full h-[200vh] relative">
+        <div className="w-full sticky top-0 left-0">
+          <div className="btntext absolute bottom-[5%] left-[3%] z-[4]">
+            <h1>
+              We build big ideas. <br />
+              Software. Apps. Tools. <br />
+              For real people. Real <br />
+              lives.
+            </h1>
+          </div>
+          <motion.div
+            style={{ clipPath: divClip }}
+            className="videos z-[3] w-full h-screen absolute top-0 left-0 overflow-hidden"
+          >
+            <video
+              muted
+              loop
+              autoPlay
+              className="absolute w-full top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2"
+              src="/Y2meta.app-THIS IS 4K MARVEL (Ultra HD)-(1080p60).mp4"
+            />
+          </motion.div>
+          <div className="marquees w-full h-screen relative overflow-hidden">
+            <div className="heading absolute top-[5%] left-1/2 -translate-x-1/2">
+              <h1 className="text-xl font-regular text-center">
+                Crafting a new paradigm of <br />
+                healthcare, one that is
+              </h1>
+            </div>
+              <div ref={ref} className="h-[300vh]">
+                <div className="sticky top-1/2 -translate-y-1/2 text-[68px] text-white overflow-clip space-y-4">
+                  <motion.div
+                    style={{ scale, x: xRow1}}
+                    className="flex gap-6 justify-center items-center"
+                  >
+                    <p>HTML</p>
+                    <Image id={1} />
+                    <p>CSS</p>
+                    <Image id={2} />
+                    <p>JavaScript</p>
+                    <Image id={3} />
+                    <p>TypeScript</p>
+                    <Image id={4} />
+                  </motion.div>
+                  <motion.div
+                    style={{ scale, x: xRow2}}
+                    className="flex gap-6 justify-center items-center"
+                  >
+                    <p>Svelte</p>
+                    <Image id={5} />
+                    <p>Vue</p>
+                    <Image id={6} />
+                    <p>React</p>
+                    <Image id={7} />
+                    <p>Angular</p>
+                    <Image id={8} />
+                  </motion.div>
+                  <motion.div
+                    style={{ scale, x: xRow3}}
+                    className="flex gap-6 justify-center items-center"
+                  >
+                    <p>Accessibility</p>
+                    <Image id={9} />
+                    <p>Performance</p>
+                    <Image id={10} />
+                    <p>SEO</p>
+                    <Image id={11} />
+                    <p>Semantics</p>
+                    <Image id={12} />
+                  </motion.div>
+                </div>
+              <div className="h-screen" />
+            </div>
+          </div>
+          <div className="button-container absolute bottom-[10%] left-[3%] z-[4]">
+            <button className="btn-primary">Learn More</button>
+          </div>
+        </div>
       </div>
     </main>
   );
 }
+
+const Image = ({ id }: { id: number }) => (
+  <img
+    className="w-16 h-16 shrink-0 rounded-full"
+    src={`https://randomuser.me/api/portraits/women/${id}.jpg`}
+    alt="Random user picture"
+  />
+);
--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,8 +1,104 @@
+/* eslint-disable @next/next/no-img-element */
+"use client";
 
-export default function Hero() {
-  return (
-    <div>
-      
+import React, { useEffect } from "react";
+import gsap from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+
+gsap.registerPlugin(ScrollTrigger);
+
+const words = [
+  { text: "empathetic", imgSrc: "https://cdn.prod.website-files.com/659dbdfd5a080be8d3483164/659dbdfd5a080be8d3483184_Ellipse%20845.png" },
+  { text: "empathetic", imgSrc: "https://cdn.prod.website-files.com/659dbdfd5a080be8d3483164/659dbdfd5a080be8d3483184_Ellipse%20845.png" },
+  { text: "empathetic", imgSrc: "https://cdn.prod.website-files.com/659dbdfd5a080be8d3483164/659dbdfd5a080be8d3483184_Ellipse%20845.png" },
+  { text: "empathetic", imgSrc: "https://cdn.prod.website-files.com/659dbdfd5a080be8d3483164/659dbdfd5a080be8d3483184_Ellipse%20845.png" },
+  { text: "useful", imgSrc: "https://cdn.prod.website-files.com/659dbdfd5a080be8d3483164/659dbdfd5a080be8d3483195_Ellipse%20845.png" },
+  { text: "useful", imgSrc: "https://cdn.prod.website-files.com/659dbdfd5a080be8d3483164/659dbdfd5a080be8d3483195_Ellipse%20845.png" },
+  { text: "useful", imgSrc: "https://cdn.prod.website-files.com/659dbdfd5a080be8d3483164/659dbdfd5a080be8d3483195_Ellipse%20845.png" },
+  { text: "useful", imgSrc: "https://cdn.prod.website-files.com/659dbdfd5a080be8d3483164/659dbdfd5a080be8d3483195_Ellipse%20845.png" },
+  { text: "useful", imgSrc: "https://cdn.prod.website-files.com/659dbdfd5a080be8d3483164/659dbdfd5a080be8d3483195_Ellipse%20845.png" },
+  { text: "intuitive", imgSrc: "https://cdn.prod.website-files.com/659dbdfd5a080be8d3483164/659dbdfd5a080be8d3483180_Ellipse%20845-4.png" },
+  { text: "intuitive", imgSrc: "https://cdn.prod.website-files.com/659dbdfd5a080be8d3483164/659dbdfd5a080be8d3483180_Ellipse%20845-4.png" },
+  { text: "intuitive", imgSrc: "https://cdn.prod.website-files.com/659dbdfd5a080be8d3483164/659dbdfd5a080be8d3483180_Ellipse%20845-4.png" },
+  { text: "intuitive", imgSrc: "https://cdn.prod.website-files.com/659dbdfd5a080be8d3483164/659dbdfd5a080be8d3483180_Ellipse%20845-4.png" },
+];
+
+const HomeSection = () => {
+  useEffect(() => {
+    gsap.set(".slidesm", { scale: 5 });
+
+    const tl = gsap.timeline({
+      scrollTrigger: {
+        trigger: ".home",
+        scrub: 1,
+        start: "top top",
+        end: "bottom bottom",
+      },
+    });
+
+    tl.to(".videos", { "--clip": "0%", ease: "power2" }, "a")
+      .to(".slidesm", { ease: "power2", scale: 1 }, "a")
+      .to(".lft", { ease: "power4", xPercent: -10 }, "b")
+      .to(".rgt", { ease: "power4", xPercent: 10, stagger: 0.0002 }, "b");
+  }, []);
+
+  const renderElements = (className : any) => (
+    <div className={`row ${className} w-full flex item-center gap-3 pb-7 whitespace-nowrap`}>
+      {words.map((word, index) => (
+        <div key={index} className="elem flex item-center gap-5">
+          <h1 className="text-5xl font-semibold">{word.text}</h1>
+          <div className="img w-[3.5rem] h-[3.5rem] rounded-full">
+            <img className="object-cover w-full h-full" src={word.imgSrc} alt="" />
+          </div>
+        </div>
+      ))}
     </div>
-  )
-}
+  );
+
+  return (
+    <div className="main w-full">
+      <div  className="home section w-full h-[200vh] relative">
+        <div className="w-full sticky top-0 left-0">
+          <div className="btntext absolute bottom-[5%] left-[3%] z-[4]">
+            <h1>
+              We build big ideas. <br />
+              Software. Apps. Tools. <br />
+              For real people. Real <br />
+              lives.
+            </h1>
+          </div>
+          <div
+            style={{ "--clip": "100%" } as React.CSSProperties}
+            className="videos z-[3] w-full h-screen absolute top-0 left-0 overflow-hidden"
+          >
+            <video
+              muted
+              loop
+              autoPlay
+              className="absolute w-full top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2"
+              src="https://cdn.significo.com/videos/significo-main-hero.mp4"
+            ></video>
+          </div>
+          <div className="marquees w-full h-screen relative overflow-hidden">
+            <div className="heading absolute top-[5%] left-1/2 -translate-x-1/2">
+              <h1 className="text-xl font-regular text-center">
+                Crafting a new paradigm of <br />
+                healthcare, one that is
+              </h1>
+            </div>
+            <div className="slidesm scale-[1] absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-[35%] w-[90%]">
+              {renderElements("lft")}
+              {renderElements("lft")}
+              {renderElements("rgt")}
+            </div>
+          </div>
+          <div className="button-container absolute bottom-[10%] left-[3%] z-[4]">
+            <button className="btn-primary">Learn More</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default HomeSection;
