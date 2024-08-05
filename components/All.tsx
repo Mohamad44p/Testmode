2d0d2dd91ee5bf1fe280b2a1a2e0ed2f05369eb6
Mohamad44p <mohamad44p@yahoo.com>
2024-08-05 13:40:43 +0300

    Traying fix scrolling

--- components/All.tsx
+++ components/All.tsx
@@ -41,21 +41,28 @@ const changeBodyBackgroundColor = () => {
 
 export default function All() {
   useEffect(() => {
-    const lenis = new Lenis();
+    const lenis = new Lenis({
+      smoothWheel: true,
+      duration: 1.2,
+      easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t)),
+    });
 
-    function raf(time: number) {
+    const raf = (time: number) => {
       lenis.raf(time);
       requestAnimationFrame(raf);
-    }
+    };
 
     requestAnimationFrame(raf);
 
     changeBodyBackgroundColor();
+
+    return () => {
+      lenis.destroy();
+    };
   }, []);
 
   return (
     <div className="main w-full">
-      {" "}
       <HomeSection />
       <section data-color="cyan">
         <CardsSec />
